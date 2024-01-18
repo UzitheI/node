deeec443f76dbd420f60ae2d729bb348c9138bef
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-18 16:11:44 +0545

    get started

---
+++ Day1/RunningNode.js
@@ -0,0 +1,9 @@
+console.log("humans are strong");
+//basically nodeJs brings the javascript running ability of the browser to the computer
+
+console.log("Humans are dumb");
+
+//we can automatically run node servers multiple times everytime we make a change in the javascript file by using 
+
+// node --watch (file name)
+
