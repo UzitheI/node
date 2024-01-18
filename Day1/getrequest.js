b0e2e9108b2fafe78112e4f0db98ed3585163acc
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-18 16:16:02 +0545

    way of axios

---
+++ Day1/getrequest.js
@@ -0,0 +1,15 @@
+//making an http request
+
+//the number 1 method to make this request is by the means of axios but axios requires a third party server
+
+// const axios=require ('axios');
+
+// axios
+// .get('https://example.com/todos')
+// .then(res=>{
+//     console.log(`statusCode:${res.status}`);
+//     console.log(res);
+// })
+// .catch(error=>{
+//     console.log(error);
+// });
