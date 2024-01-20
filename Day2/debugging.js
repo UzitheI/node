2b3dee8e333cc898dbf1b16840f2a4d17b7ae579
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-20 15:49:46 +0545

    debugger vs code

---
+++ Day2/debugging.js
@@ -0,0 +1,12 @@
+//learning about debuggers is crucial
+
+var i=1;
+function human(){
+    setInterval(()=>{
+        console.log('Iteration'+i);
+        i++;
+    },2000)
+}
+human();
+
+//only sample code to try built in vs code debugger  
