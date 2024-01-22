1345c307acc55a1fbc3a7e06ede224c40c894993
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-22 17:45:20 +0545

    how to use mongo shell

---
+++ Mongo/mongsh.js
@@ -0,0 +1,15 @@
+//we can communicate with mongodb with our shell as well
+
+
+//when we are using shell, we start by being in the test database which doesnt really exist, in a shell, it donest matter if the shell doesnt exist. if we add data and collection inside the database, then it gets created, before that it technically doesnt exist
+
+/*
+show dbs
+use bookstore
+cls
+current db-db 
+show collections
+using var to write variables
+help
+exit
+*/
