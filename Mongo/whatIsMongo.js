7857aa2b02ade3930058dd8d0d1c81c8e4c86ada
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-22 17:45:34 +0545

    info

---
+++ Mongo/whatIsMongo.js
@@ -0,0 +1,31 @@
+//atlas has database as a service which handles all lifecycles on its own
+
+//serverless and clusters
+
+//clusters is where multiple servers work together while serverless are good for low trafic 
+
+//clusters are still used for large scale production because it provides options regarding your provider like Azure, Google Cloud
+
+//sql vs no sql
+
+//sql is relational while something like mongoDB is no sql which means it uses collection and documents
+
+//when we use sql we create tables and these tables might have many other chlidren table with them and we can create relations with them 
+
+//with MONGOdb we can create json like objects to make it more readable
+
+/*
+{
+
+    name:"ujjwal puri",
+    age:18,
+    customerID: 234234,
+    fee-details:{
+        "money":3000,
+        "last-name":"michael"
+    }
+
+    }
+*/
+
+
