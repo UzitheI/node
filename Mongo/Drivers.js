92783e6d16186a829d01c0ca67e0c1e8bd8b2ffe
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-26 22:51:21 +0545

    drivers in mongo

---
+++ Mongo/Drivers.js
@@ -0,0 +1,6 @@
+ //we are not using any programming language before this to interact with the data 
+
+ //thus we use drivers available for different programming language in order to communicate with the mongoDB database from the code 
+
+
+ 
