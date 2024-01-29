ad9d49d1411179263a3ad41c46b3a9d1b644e40b
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-29 21:20:45 +0545

    export modules

---
+++ Express/expt.js
@@ -0,0 +1,10 @@
+// expt.js
+
+// Example implementation, replace this with your actual implementation
+const perimeter = (side) => 4 * side;
+const area = (side) => side * side;
+
+module.exports = {
+  perimeter,
+  area,
+};
